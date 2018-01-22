095f9ab588f011fe8216b28d4ce3dec1b64d3e10
dcj <940531558@qq.com>
2018-01-22 09:32:47 +0800

    初次添加

---
+++ dubbo_tree_consumer/src/main/java/com/jk/controller/WER.java
@@ -0,0 +1,16 @@
+package com.jk.controller;
+
+/**
+ * &lt;pre&gt;项目名称：dubbo_tree_main
+ * 类名称：${TYPE_NAME}
+ * 类描述：
+ * 创建人：丁成杰  Email -904531558@qq.com
+ * 创建时间：2018/1/22 9:05
+ * 修改人：丁成杰 Email -904531558@qq.com
+ * 修改时间：2018/1/22 9:05
+ * 修改备注：
+ *
+ * @version ${ENCLOSING_TYPE}${TAGS}&lt;/pre&gt;
+ */
+public class WER {
+}
